96526607dc342c28901d7704c68fbcb18ddfc884
tranquoccuong267 <36974582+tranquoccuong267@users.noreply.github.com>
2018-06-02 09:42:21 +0700

    cuong dep trai

--- app/src/main/java/com/nguyenquanghuy605/bookyardfootball/View/ListAllYard.java
+++ app/src/main/java/com/nguyenquanghuy605/bookyardfootball/View/ListAllYard.java
@@ -34,26 +34,26 @@ public class ListAllYard extends AppCompatActivity {
         lvYard.setAdapter(yardAdapter);
     }
     private void AnhXa(){
-        lvYard = (ListView) findViewById(R.id.listviewYard);
-        //btnBookYard  = (Button) findViewById(R.id.btnBookYard);
-        yardArrayList  = new ArrayList<>();
+            lvYard = (ListView) findViewById(R.id.listviewYard);
+            //btnBookYard  = (Button) findViewById(R.id.btnBookYard);
+            yardArrayList  = new ArrayList<>();
 
-        yardArrayList.add(new Yard("001", "Sân SPKT", R.drawable.sandaihoc,"Ho Chi Minh", "6"));
-        yardArrayList.add(new Yard("002", "Sân Hiệp Phú", R.drawable.sanbong,"Ho Chi Minh", "4"));
-        yardArrayList.add(new Yard("003", "Sân Thiếu Nhi", R.drawable.santhieunhi,"Ho Chi Minh", "3"));
-        yardArrayList.add(new Yard("004", "Sân Quốc Tế", R.drawable.santruong,"Ho Chi Minh", "8"));
-        yardArrayList.add(new Yard("005", "Sân Bóng AT", R.drawable.sanat,"Ho Chi Minh", "3"));
-        yardArrayList.add(new Yard("006", "Sân Việt Thắng", R.drawable.sanvietthang,"Ho Chi Minh", "2"));
-        yardArrayList.add(new Yard("007", "Sân Cao Đẳng", R.drawable.sanbonghiepphu,"Ho Chi Minh", "3"));
+            yardArrayList.add(new Yard("001", "Sân SPKT", R.drawable.sandaihoc,"Ho Chi Minh", "6"));
+            yardArrayList.add(new Yard("002", "Sân Hiệp Phú", R.drawable.sanbong,"Ho Chi Minh", "4"));
+            yardArrayList.add(new Yard("003", "Sân Thiếu Nhi", R.drawable.santhieunhi,"Ho Chi Minh", "3"));
+            yardArrayList.add(new Yard("004", "Sân Quốc Tế", R.drawable.santruong,"Ho Chi Minh", "8"));
+            yardArrayList.add(new Yard("005", "Sân Bóng AT", R.drawable.sanat,"Ho Chi Minh", "3"));
+            yardArrayList.add(new Yard("006", "Sân Việt Thắng", R.drawable.sanvietthang,"Ho Chi Minh", "2"));
+            yardArrayList.add(new Yard("007", "Sân Cao Đẳng", R.drawable.sanbonghiepphu,"Ho Chi Minh", "3"));
 
-        lvYard.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(ListAllYard.this, "Huy ", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(ListAllYard.this, SearchYard.class);
-                startActivity(intent);
-            }
-        });
+            lvYard.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Toast.makeText(ListAllYard.this, "Huy ", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(ListAllYard.this, SearchYard.class);
+                    startActivity(intent);
+                }
+            });
 
 //        btnBookYard.setOnClickListener(new View.OnClickListener() {
 //            @Override
